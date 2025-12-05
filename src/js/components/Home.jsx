46654e7669557f5eb434f90cb095b5b3c87fa738
IkerMoBu <92671501+Ikerichu@users.noreply.github.com>
2025-12-05 15:07:14 +0000

    Fin

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [tarea, setTarea] = useState('');
+	const [listaTareas, setListaTareas] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const updateTarea = (prop) => {
+		const newId = listaTareas.length + 1;
+		const newTarea = {
+			id: newId,
+			chore: prop
+		}
+		setListaTareas([...listaTareas, newTarea]);
+		setTarea("");
+		console.log(listaTareas);
+	}
+	const borrarTarea = (id) => {
+		setListaTareas(listaTareas.filter(item => item.id !== id));
+	};
+
+
+	return (
+		<div className="container-fluid bg-dark">
+			<div className="d-flex flex-column justify-content-center align-items-center p-3 m-4 mt-0">
+				<input
+					className="w-50"
+					type="text"
+					placeholder="Type your next chore"
+					value={tarea}
+					onChange={(elem) => setTarea(elem.target.value)} />
+				<button
+					type="button"
+					className="btn btn-secondary w-50 p-1 m-3"
+					onClick={() => {
+						updateTarea(tarea)
+					}
+					}>
+					Add chore
+				</button>
+
+
+			</div>
+			<div className="d-flex flex-column justify-content-center align-items-center p-3 m-4 mt-0">
+
+				{listaTareas.map(item => (
+
+					<div key={item.id} className="d-flex w-50 text-light bg-dark justify-content-between align-items-center m-1">
+						<span><strong>{item.chore}</strong></span>
+						<button
+							type="button"
+							className="btn btn-light text-dark p-1 flex-end"
+							onClick={()=>borrarTarea(item.id)}>
+							x
+						</button>
+					</div>
+				))}
+
+
+
+			</div>
+		</div >
 	);
 };
 
